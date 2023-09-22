f798bb98814ab9b536897ff53da0565d1fc928a2
tubo28 <3405886+tubo28@users.noreply.github.com>
2023-09-23 00:39:31 +0900

    s/merge_/merge/

--- src/examples/mergesort.lisp
+++ src/examples/mergesort.lisp
@@ -9,20 +9,20 @@
     ((> begin 0) (subseq (cdr lst) (- begin 1) (- end 1)))
     (t (cons (car lst) (subseq (cdr lst) 0 (- end 1))))))
 
-(defun merge_ (f s)
+(defun merge (f s)
   (cond
     ((= (length f) 0) s)
     ((= (length s) 0) f)
-    ((< (car f) (car s)) (append (list (car f)) (merge_ (cdr f) s)))
-    ((> (car f) (car s)) (append (list (car s)) (merge_ f (cdr s))))
-    ((= (car f) (car s)) (append (list (car f) (car s)) (merge_ (cdr f) (cdr s))))))
+    ((< (car f) (car s)) (append (list (car f)) (merge (cdr f) s)))
+    ((> (car f) (car s)) (append (list (car s)) (merge f (cdr s))))
+    ((= (car f) (car s)) (append (list (car f) (car s)) (merge (cdr f) (cdr s))))))
 
 (defun merge-sort(lst)
   (let ((len (length lst)))
     (cond
       ((= len 1) lst)
       (t
-      (merge_ (merge-sort (subseq lst 0 (/ len 2)))
+      (merge (merge-sort (subseq lst 0 (/ len 2)))
               (merge-sort (subseq lst (/ len 2) len)))))))
 
 (merge-sort '(3 1 4 1 5 9 2 6 5 3 5 8 9 7 9))
