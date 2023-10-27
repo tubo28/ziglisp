2b23d4be98498d2fc9667870b776566db6634484
tubo28 <3405886+tubo28@users.noreply.github.com>
2023-10-27 14:54:06 +0900

    Simplify function call

--- src/eval.zig
+++ src/eval.zig
@@ -13,7 +13,6 @@ const Value = C.Value;
 const ValueRef = C.ValueRef;
 
 const Token = @import("tok.zig").Token;
-
 const Builtin = @import("builtin.zig");
 
 pub fn evaluate(x: ValueRef, env: EnvRef) anyerror!EvalResult {
@@ -22,21 +21,11 @@ pub fn evaluate(x: ValueRef, env: EnvRef) anyerror!EvalResult {
         Value.number => return .{ x, env },
         Value.lambda, Value.b_func, Value.b_form => unreachable,
         Value.symbol => return if (En.resolve(env, x)) |ent| .{ ent, env } else .{ x, env },
-        Value.cons => |cons| {
-            // Call something
-            const c = try toCallable(cons.car, env);
-            return call(c, cons.cdr, env);
-        },
+        Value.cons => |cons| return call(cons.car, cons.cdr, env),
     }
 }
 
-const Callable = union(enum) {
-    b_form: *const Builtin.SpecialForm,
-    b_func: *const Builtin.Function,
-    lambda: *const Lambda,
-};
-
-fn toCallable(car: *const C.Value, env: EnvRef) !Callable {
+fn call(car: *const C.Value, args: ValueRef, env: EnvRef) anyerror!EvalResult {
     if (car.* != .cons and car.* != .symbol) {
         std.log.err("not callable: {}", .{car.*});
         unreachable;
@@ -45,22 +34,21 @@ fn toCallable(car: *const C.Value, env: EnvRef) !Callable {
     if (car.* == .cons) {
         // example: ((lambda (x) (+ x x)) 1)
         const lmd, _ = try evaluate(car, env);
-        return Callable{ .lambda = lmd.lambda };
+        return callLambda(lmd.lambda, args, env);
     }
 
+    // car.* == .symbol
     const name = car;
 
-    if (En.resolve(env, name) == null) {
+    const callee = En.resolve(env, name);
+    if (callee == null) {
         std.log.err("not callable value: {s} {}", .{ S.getName(name.symbol).?, car.* });
         unreachable;
     }
-
-    const callable = En.resolve(env, name).?;
-
-    switch (callable.*) {
-        Value.lambda => |l| return Callable{ .lambda = l },
-        Value.b_func => |bf| return Callable{ .b_func = Builtin.func[bf] },
-        Value.b_form => |bs| return Callable{ .b_form = Builtin.form[bs] },
+    switch (callee.?.*) {
+        Value.lambda => |lambda| return callLambda(lambda, args, env),
+        Value.b_func => |func| return callFunc(Builtin.func[func], args, env),
+        Value.b_form => |form| return callForm(Builtin.form[form], args, env),
         else => |other| {
             std.log.err("symbol `{s}` is bound to non-callable value: {any}", .{ S.getName(name.symbol).?, other });
             unreachable;
@@ -68,24 +56,24 @@ fn toCallable(car: *const C.Value, env: EnvRef) !Callable {
     }
 }
 
-fn call(callable: Callable, args: ValueRef, env: EnvRef) anyerror!EvalResult {
-    switch (callable) {
-        Callable.b_form => |form| return form(args, env),
-        Callable.b_func => |func| return .{ try func(try evalAll(args, env)), env },
-        Callable.lambda => |lambda| return .{ try callLambda(lambda, try evalAll(args, env)), env },
-    }
+fn callForm(form: *const Builtin.SpecialForm, args: ValueRef, env: EnvRef) anyerror!EvalResult {
+    return form(args, env);
 }
 
-fn evalAll(xs: ValueRef, env: EnvRef) !ValueRef {
+fn callFunc(func: *const Builtin.Function, args: ValueRef, env: EnvRef) anyerror!EvalResult {
+    return .{ try func(try evalAll(args, env)), env };
+}
+
+fn callLambda(lambda: *const Lambda, args: ValueRef, env: EnvRef) anyerror!EvalResult {
+    var lambda_env = try M.addAll(lambda.closure, lambda.params, try evalAll(args, env));
+    const ret, _ = try evaluate(lambda.body, lambda_env);
+    return .{ ret, env };
+}
+
+fn evalAll(xs: ValueRef, env: EnvRef) anyerror!ValueRef {
     std.debug.assert(xs.* == .cons);
     if (xs == C.empty()) return xs;
     const car, _ = try evaluate(xs.cons.car, env);
     const cdr = try evalAll(xs.cons.cdr, env);
     return try C.newCons(car, cdr);
-}
-
-fn callLambda(lambda: *const Lambda, args: ValueRef) anyerror!ValueRef {
-    var lambda_env = try M.addAll(lambda.closure, lambda.params, args);
-    const ret, _ = try evaluate(lambda.body, lambda_env);
-    return ret;
 }
